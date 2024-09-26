808e87346ef845e58b1b6ec5d9e00f09f6d86d37
ployloyloy <ployloyloy06@gmail.com>
2024-09-27 00:44:13 +0700

    Update main.go

--- main.go
+++ main.go
@@ -4,35 +4,6 @@ import "fmt"
 
 func main(){
 	
-	// fmt.Println("Hello World!")
-
-	// var nameOne string = "mario"
-	// var nameTwo = "luigi"
-	// var nameThree string
-	// fmt.Println(nameOne, nameTwo, nameThree)
-
-	// nameOne = "peach"
-	// nameThree = "bowser"
-	// fmt.Println(nameOne, nameTwo, nameThree)
-
-	nameFour := "yoshi"
-	fmt.Println(nameFour)
-
-	//ints
-	var ageOne int = 20
-	var ageTwo = 30
-	ageThree := 40
-	fmt.Println(ageOne, ageTwo, ageThree)
-
-	//bits & memory
-	// var numOne int8 = 15
-	// var numTwo int8 = -128
-	// var numOneThree uint16 = 256
-
-	// var scoreOne float32 = 25.98
-	// var scoreTwo float64 = 4308454354353049
-	// scoreThree := 1.5
-
-
+	fmt.Println("Hello World!")
 
 }
