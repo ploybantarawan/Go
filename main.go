617f8aa5eb0f3d11fff4da9b30c7f2d43170823f
ployloyloy <ployloyloy06@gmail.com>
2024-09-29 17:07:24 +0700

    Update main.go

--- main.go
+++ main.go
@@ -4,6 +4,29 @@ import "fmt"
 
 func main(){
 	
-	fmt.Println("Hello World!")
+	// var ages [3]int = [3]int{20, 25, 30} //fixed length
+	var ages = [3]int{20, 25, 30}
 
+	names := [4]string{"yoshi", "mario", "peach", "bowser"}
+	names[1] = "luigi"
+
+	fmt.Println(ages, len(ages))
+	fmt.Println(names, len(names))
+
+	//slices (use arrays under the hood)
+	var scores = []int{100,50,60}
+	scores[2] = 25
+	scores = append(scores, 85)
+
+	fmt.Println(scores, len(scores))
+
+	//slice ranges
+	rangeOne := names[1:3]
+	rangeTwo := names[2:]
+	rangeThree := names[:3]
+
+	fmt.Println(rangeOne, rangeTwo, rangeThree)
+
+	rangeOne = append(rangeOne, "kooper")
+	fmt.Println(rangeOne)
 }
