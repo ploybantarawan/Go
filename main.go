4f53b53b4faa630b042ed23a961cd2d3e5cb4aa0
ployloyloy <ployloyloy06@gmail.com>
2024-09-29 18:40:30 +0700

    Update main.go

--- main.go
+++ main.go
@@ -4,6 +4,26 @@ import "fmt"
 
 func main(){
 	
-	fmt.Println("Hello World!")
+	// x := 0
+	// for x < 5 {
+	// 	fmt.Println("the value of x is:", x)
+	// 	x++
+	// }
 
-}
+	// for i := 0; i<5; i++{
+	// 	fmt.Println("the value of i is:", i)
+	// }
+
+	names := []string{"ploy", "meen", "ice", "fah"}
+
+	// for i := 0; i<len(names); i++{
+	// 	fmt.Println(names[i])
+	// }
+
+	for _, value := range names{
+		 fmt.Printf("the values is %v \n", value)
+	}
+
+	fmt.Println(names)
+
+} 
