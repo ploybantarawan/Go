dee7e2c8e9e2c129d0411db12105bebb0d2474b0
ployloyloy <ployloyloy06@gmail.com>
2024-09-29 16:46:38 +0700

    Update main.go

--- main.go
+++ main.go
@@ -3,7 +3,25 @@ package main
 import "fmt"
 
 func main(){
-	
-	fmt.Println("Hello World!")
 
+	age := 23
+	name := "ploy"
+	
+	fmt.Print("Hello, ")
+	fmt.Print("world! \n")
+	fmt.Print("new line \n")
+
+	fmt.Println("hello ploy")
+	fmt.Println("bye ploy")
+	fmt.Println("my age is", age, "and my name is", name)
+
+	//Printf (formatted strings) %_ = formatted specifier
+	fmt.Printf("my age is %v and my name is %v \n", age, name)
+	fmt.Printf("my age is %q and my name is %q \n", age, name)
+	fmt.Printf("age is of type %T \n", age)
+	fmt.Printf("you scored %0.1f points! \n", 255.55)
+
+	// Sprint (save formatted strings)
+	var str = fmt.Sprintf("my age is %v and my name is %v \n", age, name)
+	fmt.Println("the saved string is:", str)
 }
